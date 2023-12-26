4dc5b4909b3f9e5aaa24b1000266ee61a5d57a4e
Chris <c.smith4804023768@yahoo.com>
2023-12-25 23:21:29 -0500

    6kyu challenge

---
+++ level 6/6kyu multiplicationtable.js
@@ -0,0 +1,27 @@
+// Your task, is to create N×N multiplication table, of size provided in parameter.
+
+// For example, when given size is 3:
+
+// 1 2 3
+// 2 4 6
+// 3 6 9
+
+// For the given example, the return value should be:
+
+// [[1,2,3],[2,4,6],[3,6,9]]
+
+
+// solution 
+
+multiplicationTable = function(size) {
+    let result = [];
+  
+    for ( i = 0; i < size; i++) {
+      result[i] = [];
+      for( j = 0; j < size; j++) {
+        result[i][j] = (i + 1) * (j + 1);
+      }
+    }
+    
+    return result
+  }
