9b3a8f5364e143e981b896e478d59a8481529496
Chris <c.smith4804023768@yahoo.com>
2023-12-30 22:33:26 -0500

    daily challenge

---
+++ level 7/7kyu noodditieshere.js
@@ -0,0 +1,11 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+
+// solution
+
+function noOdds(num){
+    return num.filter(num => num % 2 === 0)
+}
+
