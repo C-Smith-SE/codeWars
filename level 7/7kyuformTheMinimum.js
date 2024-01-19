5eeb2791c605b618840e82203cf6f9efbf031ab9
Chris <c.smith4804023768@yahoo.com>
2024-01-18 23:29:26 -0500

    daily challenge

---
+++ level 7/7kyuformTheMinimum.js
@@ -0,0 +1,21 @@
+// Task
+
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+// Notes:
+
+//     Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+//     Input >> Output Examples
+
+// minValue ({1, 3, 1})  ==> return (13)
+
+
+// solution 
+
+function minValue(values){
+    const n = values
+      .filter((e, i, arr) => arr.indexOf(e) === i)
+      .sort()
+      .join('')
+    return Number(n)
+  }
+
