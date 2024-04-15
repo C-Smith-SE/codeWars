ed752ff33a0e73a6a79038d3b88e6fd988d2d556
Chris <c.smith4804023768@yahoo.com>
2024-04-14 23:54:18 -0400

    daily challenge

---
+++ level 6/6kyu alternatingLoops.js
@@ -0,0 +1,27 @@
+// Write
+
+// function combine()
+
+// that combines arrays by alternatingly taking elements passed to it.
+
+// E.g
+
+// combine(['a', 'b', 'c'], [1, 2, 3]) == ['a', 1, 'b', 2, 'c', 3]
+// combine(['a', 'b', 'c'], [1, 2, 3, 4, 5]) == ['a', 1, 'b', 2, 'c', 3, 4, 5]
+// combine(['a', 'b', 'c'], [1, 2, 3, 4, 5], [6, 7], [8]) == ['a', 1, 6, 8, 'b', 2, 7, 'c', 3, 4, 5]
+
+// Arrays can have different lengths.
+ 
+
+// solution 
+
+function combine(...arrs) {
+    let max = arrs.reduce((n, arr) => Math.max(n, arr.length), 0)
+    let res = []
+    for (let i = 0; i < max; i++) {
+      for (let arr of arrs) {
+        if (i in arr) res.push(arr[i])
+      }
+    }
+    return res
+  }
